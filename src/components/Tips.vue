3026b999a6e0df3879c8cec9323ce4b96f568cba
subdavis <brandonadavis@outlook.com>
2018-05-12 17:39:47 -0400

    WIP - tips

---
+++ src/components/Tips.vue
@@ -0,0 +1,27 @@
+<!-- TIPS is a UI for helping the user learn to navigate Tusk -->
+<template>
+	<div class="box-bar selectable" v-on:click="$router.goBack">
+		<span>
+      <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
+      {{ message }}
+    </span>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+            settings: Object
+        },
+        data () {
+            return {}
+        },
+        mounted() {
+            // 
+        }
+	}
+</script>
+
+<style lang="scss">
+	@import "../styles/settings.scss";
+</style>
